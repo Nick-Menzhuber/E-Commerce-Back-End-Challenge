788c44fbbba629de9a201e8324ffd45607f3c305
Nick Menzhuber <nickmenzhuber@Nicks-Mac-mini.local>
2022-12-01 17:20:56 -0600

    finally got find all categories route to work

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,31 +2,31 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    product_name: 'Star Wars T-Shirt',
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    product_name: 'Nike Cortez Sneakers',
     price: 90.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
+    product_name: 'Minnesota Twins Hat',
     price: 22.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
-    price: 12.99,
+    product_name: 'Sgt Pepper\'s Lonely Hearts Club Band',
+    price: 19.67,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
+    product_name: 'Dad Cargo Shorts',
     price: 29.99,
     stock: 22,
     category_id: 2,
